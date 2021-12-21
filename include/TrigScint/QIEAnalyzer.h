b73dbd5ea93b011befa9f8c69657f1ba02cad558
GNiramay <niramaygogate@gmail.com>
2021-12-21 01:22:27 -0600

    Debugging work, before ldmx-sw image printing related fix

--- include/TrigScint/QIEAnalyzer.h
+++ include/TrigScint/QIEAnalyzer.h
@@ -98,6 +98,7 @@ namespace trigscint {
     TH1F* hOut[nEv][nChannels];;
     TH1F* hPE[nChannels];
     TH2F* hPEvsT[nChannels];
+    TH2F* hAllPulses[nChannels];
 
     TH2F* hAvgQiQj[nChannels*(nChannels-1)/2]; // Integrated charge correlation plots
     TH2F* hQiQj[nChannels*(nChannels-1)/2]; // charge correlation plots
@@ -106,10 +107,11 @@ namespace trigscint {
     TH1F* Rel_Dev;				      // Relative deviation in charge deposition
     TH2F* Abs_Dev;				      // Absoulte deviation w.r.t. median
     TH2F* hGoodPulses;	// Pulse shapes with Qmed<300
+    
     TH1F* AllNoise;	// Charge in 1st 15 ts
     TH1F* Q15_weighted;	// Charge in 1st 15 ts, weighted by sensitivity
-    TH1F* hQAvg15;	// Average Q in 1st 15 ts
-    TH1F* hQAvg1530;	// Average Q in last 15 ts
+    TH1F* hQAvg15[nChannels];	// Average Q in 1st 15 ts
+    TH1F* hQAvg1530[nChannels];	// Average Q in last 15 ts
     
     TH1F* hPhot1Pulse[100];	// per event pulse shape
     int photcount{0};		// No. of single photon events passed
@@ -124,9 +126,21 @@ namespace trigscint {
     TH1F* hPhot1Ped;
     TH1F* hPhot1Chi;
 
+    TH1F* hPhot1Trial3K[3];	// k's calculated from trial 3
+    TH1F* hPhot1Trial3KAvg;	// Average k's calculated from trial 3
+
+    TH1F* hQ18Avg[2];		// 2 hists for LYSO and Plastic respectively.
+
+    int Q400Count{0};		// No. of single photon events passed
+    TH1F* hQ400Freq[2];		// Frequency of getting 400fC charge per time sample
+    TH1F* hQ400Pulses[100];	// Pulses with avg. charge around 400fC.
+    TH2F* hQ400Pulse_Distribution[2]; // Collection of all the pulses. LYSO and Plastic
+    TH1F* hQ400QAvg[2];		      // Average charge distribution for 420<Q15Avg<430
+
     double yOffset_{35.};
     double yToIDfactor_{50./80.};
 
+    void FitSinglePulse(Float_t* Qi);	// Fit a single pulse using 6 time samples
   };
 }
 
--- src/TrigScint/QIEAnalyzer.cxx
+++ src/TrigScint/QIEAnalyzer.cxx
@@ -33,6 +33,7 @@ namespace trigscint {
   // Temporarily store charge per time sample
   float charge[15];
   /// array of integers 0,1,2,..,maxTS-1
+  // float time[15];
   float time[15];
 
   /*
@@ -41,8 +42,6 @@ namespace trigscint {
    * par[1] = start time of the pulse (t0)
    * par[2] = 1/RC time constant (k)
    * par[3] = time when pulse attains maximum (tmax)
-   * par[4] = pedestal
-   * par[5] = noise
    */
   Double_t CostFunction(const double* par);
   
@@ -77,6 +76,7 @@ namespace trigscint {
     std::vector<float> Digi_weight[nChannels];
     std::vector<float> QErr[nChannels];
     float AvgQs[nChannels];
+    
     for (auto chan : channels) {
       std::vector<float> q = chan.getQ();
       int nTimeSamp = q.size();
@@ -87,8 +87,11 @@ namespace trigscint {
       AvgQs[bar] = chan.getAvgQ();
       float qTot = 0;
       int firstT = -1;
+      int nQ400{0};
       for (int iT = 0; iT < q.size() ; iT++) {
 	Double_t temp = smq->QErr(AllQs[bar][iT])*AllQs[bar][iT];
+	if(420<AllQs[bar][iT] && AllQs[bar][iT]<430)
+	  nQ400++;
 	QErr[bar][iT] = temp;
 	if(temp>0) Digi_weight[bar][iT] = 1/temp;
 	else Digi_weight[bar][iT] = 0;
@@ -111,9 +114,13 @@ namespace trigscint {
 	  PulseShape->Fill(ts,AllQs[bar][ts]);
 
       float QAvg15=0;
+      float QAvg18=0;
       float QAvg1530=0;
-      for(int ts=0;ts<30;ts++)
+      
+      for(int ts=0;ts<30;ts++){
 	AllPulses->Fill(ts,AllQs[bar][ts]);
+	hAllPulses[bar]->Fill(ts,AllQs[bar][ts],Digi_weight[bar][ts]);
+      }
       for(int ts=0;ts<15;ts++){
 	AllNoise->Fill(AllQs[bar][ts]);
 	
@@ -122,10 +129,30 @@ namespace trigscint {
 	//   Q15_weighted->Fill(AllQs[bar][ts],1.0/err);
 	  Q15_weighted->Fill(AllQs[bar][ts],Digi_weight[bar][ts]);
 	QAvg15+=AllQs[bar][ts];
+	QAvg18+=AllQs[bar][ts];
 	QAvg1530+=AllQs[bar][ts+15];
       }
-      hQAvg15->Fill(QAvg15/15.0);
-      hQAvg1530->Fill(QAvg1530/15.0);
+
+      for(int ts=15;ts<18;ts++)
+	QAvg18+=AllQs[bar][ts];
+
+      hQAvg15[bar]->Fill(QAvg15/15.0);
+      hQAvg1530[bar]->Fill(QAvg1530/15.0);
+      hQ18Avg[1-bar%2]->Fill(QAvg18/18.0);
+
+      if(420<(QAvg15/15.0) && (QAvg15/15.0)<430) {
+	hQ400Freq[1-bar%2]->Fill(nQ400);
+	hQ400QAvg[1-bar%2]->Fill(AvgQs[bar]);
+	
+	if(Q400Count<100){
+	  for(int ts=0;ts<nTimeSamp;ts++)
+	    hQ400Pulses[Q400Count]->Fill(ts,AllQs[bar][ts]);
+	  Q400Count++;
+	}
+	
+	for(int ts=0;ts<nTimeSamp;ts++)
+	  hQ400Pulse_Distribution[1-bar%2]->Fill(ts,AllQs[bar][ts],Digi_weight[bar][ts]);
+      }
       
       // Single photon event
       bool GoodPhot1Event=false;
@@ -138,72 +165,141 @@ namespace trigscint {
 	  for(int ts=0;ts<15;ts++)
 	    hQ15Phot1->Fill(AllQs[bar][ts]);
 	  
-	  if(GoodPhot1Event && (photcount<100)) {
-	    // // Fit pulse to single-PE events
-	    // auto min = ROOT::Math::Factory::CreateMinimizer("Minuit","Migrad");
-	    // ROOT::Math::Functor f(&CostFunction,6);
-	    // min->SetFunction(f);
-	    
-	    // min->SetVariable(0,"Q0",400.0,600.0);
-	    // min->SetVariable(1,"T0",0.,350.); // T0
-	    // min->SetVariable(2,"K",0.,5.); // K
-	    // min->SetVariable(3,"TMax",0.,100.); // TMax
-	    // min->SetVariable(4,"Pedestal",-5.,430.); // Pedestal
-	    // min->SetVariable(5,"Noise",0.,20.); // Noise
-	    
-	    // min->SetMaxFunctionCalls(1000000); // for Minuit/Minuit2 
-	    // min->SetMaxIterations(10000);  // for GSL
-	    // min->SetTolerance(0.001);
-	    // min->Minimize();
-	    // min->PrintResults();
-	    
-	    auto tg = new TGraph(15,time,&AllQs[bar][0]);
-	    // TGraphErrors* tg = new TGraphErrors(maxTS,time,&AllQs[bar][0],zeroes,&QErr[bar][0]);
+	  // if(GoodPhot1Event && (photcount<100)) {
+	  // if(GoodPhot1Event) {
+	  
+	    // // // Fit pulse to single-PE events
+	    // // for(int i=0;i<15;i++)
+	    // //   charge[i] = AllQs[bar][i];
+	    // // auto min = ROOT::Math::Factory::CreateMinimizer("Minuit","Migrad");
+	    // // ROOT::Math::Functor f(&CostFunction,6);
+	    // // min->SetFunction(f);
 
-	    TCanvas* tc = new TCanvas(Form("c_evt_%i",photcount),"bb",800,600);
-	    tg->Draw("AP");
-	    tg->SetMarkerStyle(21);
-	    TF1* func = new TF1("myfunc",SinglePulseShape,0,15,5);
-	    func->SetParNames("Q0","T0","K","TMax","Ped");
+	    // // // double temp_pars[6]={400,30,0.01,20.,10,0};
+	    // // // std::cout<<CostFunction(temp_pars)<<std::endl;
+	    // // min->SetMaxFunctionCalls(1000000); // for Minuit/Minuit2 
+	    // // // min->SetMaxIterations(10000);  // for GSL
+	    // // min->SetMaxIterations(10);  // for GSL
+	    // // min->SetTolerance(0.001);
 
-	    func->SetParameter(0,530); // Q0
-	    func->SetParameter(1,50);   // T0
-	    func->SetParameter(2,0.03); // K
-	    func->SetParameter(3,20.0);	// TMax
-	    func->SetParameter(4,1.0);	// Ped
-
-	    // func->SetParameter(0,500); // Q0
-	    // func->SetParameter(1,6);   // T0
-	    // func->SetParameter(2,0.03); // K
-	    // func->SetParameter(3,1);	// TMax
-	    // func->SetParameter(4,10);	// Ped
-
-	    tg->Fit(func);
+	    // // min->SetVariable(0,"Q0",400.0,1.0);
+	    // // min->SetVariable(1,"T0",200.,0.1); // T0
+	    // // min->SetVariable(2,"K",0.001,0.0001); // K
+	    // // min->SetVariable(3,"TMax",30,0.01); // TMax
+	    // // min->SetVariable(4,"Pedestal",0.,0.01); // Pedestal
+	    // // min->SetVariable(5,"Noise",0.,0.001); // Noise
 	    
-	    tc->SaveAs(Form("PhotFit_%i.png",photcount));
-	    tc->SaveAs("hahalol.png");
-	    delete tc;
+	    // // min->PrintResults();
+	    // // min->Minimize();
+	    // // min->SetPrintLevel(1);
+	    // // min->PrintResults();
 	    
-	    hPhot1Q0->Fill(func->GetParameter(0));
-	    hPhot1T0->Fill(func->GetParameter(1));
-	    hPhot1K->Fill(func->GetParameter(2));
-	    hPhot1TMax->Fill(func->GetParameter(3));
-	    hPhot1Ped->Fill(func->GetParameter(4));
-	    hPhot1Chi->Fill(func->GetChisquare());
+	    // auto tg = new TGraph(6);
+	    // // auto tg = new TGraph(15,time,&AllQs[bar][0]);
+	    // // TGraphErrors* tg = new TGraphErrors(maxTS,time,&AllQs[bar][0],zeroes,&QErr[bar][0]);
 
+	    // // Find maximum of pulse
+	    // int t1 = 0;		// First time sample to consider
+	    // int t2 = 14;	// Last tim sample to consider
+	    // float QMax15=AllQs[bar][0]; // Pulse maximum
+	    
+	    // for(int i=1;i<12;i++) {
+	    //   if(AllQs[bar][i]>QMax15) {
+	    // 	QMax15 = AllQs[bar][i];
+	    // 	t1 = i-1;
+	    // 	t2 = i+4;
+	    //   }
+	    // }
+
+	    // for(int i=t1;i<=t2;i++){
+	    //   tg->SetPoint(i-t1,time[i],AllQs[bar][i]);
+	    // }
+	    // TCanvas* tc = new TCanvas(Form("c_evt_%i",photcount),"bb",800,600);
+	    // tg->Draw("AP");
+	    // tg->SetMarkerStyle(21);
+	    // // TF1* func = new TF1("myfunc",SinglePulseShape,0,15,5);
+	    // // TF1* func = new TF1("myfunc",SinglePulseShape,t1,t2,5);
+	    // // TF1* func = new TF1("myfunc",SinglePulseShape,25*t1-1,25*t2+1,5);
+	    // TF1* func = new TF1("myfunc",SinglePulseShape,0,375,5);
+	    // func->SetParNames("Q0","T0","K","TMax","Ped");
+	    // printf("photcount = %d\n",photcount);
+	    
+	    // // func->SetParameter(0,530); // Q0
+	    // // func->SetParameter(1,50);   // T0
+	    // // func->SetParameter(2,0.03); // K
+	    // // func->SetParameter(3,20.0);	// TMax
+	    // // func->SetParameter(4,1.0);	// Ped
+	    
+	    // // func->SetParameters(400,200,0.001,20,1.0);
+	    // func->SetParameters(2*QMax15,25*t1-5,0.001,40,1.0);
+
+	    // // func->SetParameter(0,500); // Q0
+	    // // func->SetParameter(1,6);   // T0
+	    // // func->SetParameter(2,0.03); // K
+	    // // func->SetParameter(3,1);	// TMax
+	    // // func->SetParameter(4,10);	// Ped
+
+	    // // tg->Fit(func,"MR");
+	    // // tg->Fit(func,"","",25*t1-1,25*t2+1);
+	    // tg->Fit(func);
+	    
+	    // tc->SaveAs(Form("PhotFit_%i.png",photcount));
+	    // tc->SaveAs("hahalol.png");
+	    // delete tc;
+
+	    // // /// Debugging
+	    // // Double_t pars[5] = {func->GetParameter(0),
+	    // // 			func->GetParameter(1),
+	    // // 			func->GetParameter(2),
+	    // // 			func->GetParameter(3),
+	    // // 			func->GetParameter(4)
+	    // // };
+	    // // for(int i=0;i<15;i++) {
+	    // //   // Double_t *x,*y;
+	    // //   // tg->GetPoint(i,x,y);
+	    // //   Double_t ii = i;
+	    // //   printf("i=%d\tx=%f\ty=%f\tdata=%f\tpred=%f\n",i,tg->GetPointX(i),tg->GetPointY(i),AllQs[bar][i],SinglePulseShape(&ii,pars));
+	    // // }
+	    // // ///
+	    
+	    // hPhot1Q0->Fill(func->GetParameter(0));
+	    // hPhot1T0->Fill(func->GetParameter(1));
+	    // hPhot1K->Fill(func->GetParameter(2));
+	    // hPhot1TMax->Fill(func->GetParameter(3));
+	    // hPhot1Ped->Fill(func->GetParameter(4));
+	    // hPhot1Chi->Fill(func->GetChisquare());
+
+	  //   // Trial 3: Fit the pulse piece-by-piece
+	  // float meank=0;
+	  // float ngood=0;
+	  // for(int i=t1+2;i<t1+5;i++){
+	  //   if(AllQs[bar][i]>0 && AllQs[bar][i+1]>0){
+	  //     float ratio = AllQs[bar][i]/AllQs[bar][i+1];
+	  //     float k_ = log(ratio)/25.0;
+	  //     // printf("K_%i = %f\t",i,k_);
+	  //     // printf("%i\n",i-t1-2);
+	  //     hPhot1Trial3K[i-t1-2]->Fill(k_);
+	  //     meank+=k_;
+	  //     ngood++;
+	  //   }
+	  // }
+	  // hPhot1Trial3KAvg->Fill(meank/ngood);
+	  
+	  // // printf("\n");
+	  // //   }
+	  // // }
+	  
+	  if(photcount<100){
+	    for(int ts=0;ts<15;ts++)
+	      hPhot1Pulse[photcount]->Fill(ts,AllQs[bar][ts]);
+	    photcount++;
 	  }
 	}
-
-	if(photcount<100){
-	  for(int ts=0;ts<15;ts++)
-	    hPhot1Pulse[photcount]->Fill(ts,AllQs[bar][ts]);
-	  photcount++;
-	}
       }
-            
+    
       Abs_Dev->Fill(chan.getMedQ(),chan.getMaxQ()-chan.getMinQ());
-    }//over channels
-
+    }
+  
     // Charge Correlation
     for(int i=1;i<nChannels;i++){
       for(int j=0;j<i;j++){
@@ -214,9 +310,10 @@ namespace trigscint {
 	hAvgQiQj[(i-1)*i/2+j]->Fill(AvgQs[i],AvgQs[j]);
       }
     }
-
-    return;
-  }
+  // }//over channels
+  
+  return;
+}
 
   // /*
   void QIEAnalyzer::onFileOpen() {
@@ -238,12 +335,27 @@ namespace trigscint {
     AllNoise = new TH1F("AllNoise","Charge distribution in 1st 15 time samples;Charge [fC];",620,-20,600);
     AllPulses = new TH2F("AllPulses","All pulses seen;Time sample; Charge[fC]",30,0,30,100,-20,5000);
     Q15_weighted = new TH1F("Q15_weighted","Charge distribution in 1st 15 time samples;Charge [fC];",620,-20,600);
-    hQAvg15 = new TH1F("hQAvg15","Average Charge distribution in 1st 15 time samples;Charge [fC];",620,-20,600);
-    hQAvg1530 = new TH1F("hQAvg1530","Average Charge distribution in last 15 time samples;Charge [fC];",620,-20,600);
     PulseShape = new TH2F("hPulse","Events with Qavg>100;time sample;Q [fC]",30,0,30,100,-20,1000);
     hGoodPulses = new TH2F("hGoodPulses","Events with Qmed<300;time sample;Q [fC]",30,0,30,100,-20,5000);
     Abs_Dev = new TH2F("hAbs_Dev",";Qmed[fC];Qmax-Qmin [fC]",100,-500,500,100,0,5000);
+    
+    hQ18Avg[0] = new TH1F("hQ18Avg_LYSO","Average charge in 1st 18ts (LYSO);Charge [fC];",620,-20,600);
+    hQ18Avg[1] = new TH1F("hQ18Avg_Plast","Average charge in 1st 18ts (Plastic);Charge [fC];",620,-20,600);
 
+    hQ400Freq[0] = new TH1F("hQ400Freq_LYSO","Occurance of charge 424fC in LYSO;No. of occurances;",31,0,31);
+    hQ400Freq[1] = new TH1F("hQ400Freq_Plast","Occurance of charge 424fC in Plastic;No. of occurances;",31,0,31);
+    hQ400Pulse_Distribution[0] = new TH2F("hQ400Pulse_Distribution_LYSO","Overall pulse shape for 420<Q15Avg<430 [LYSO];time sample;Charge [fC]",31,0,31,102,-20,1000);
+    hQ400Pulse_Distribution[1] = new TH2F("hQ400Pulse_Distribution_Plast","Overall pulse shape for 420<Q15Avg<430 [Plastic];time sample;Charge [fC]",31,0,31,102,-20,1000);
+    hQ400QAvg[0] = new TH1F("hQ400QAvg_LYSO","Average Charge for events with 420<Q15Avg<430 [LYSO];Average Charge [fC];",1000,300,500);
+    hQ400QAvg[1] = new TH1F("hQ400QAvg_Plast","Average Charge for events with 420<Q15Avg<430 [Plastic];Average Charge [fC];",1000,300,500);
+
+    // Trial 3
+    hPhot1Trial3K[0] = new TH1F("hPhot1Trial3K_0","",100,0,0.3);
+    hPhot1Trial3K[1] = new TH1F("hPhot1Trial3K_1","",100,0,0.3);
+    hPhot1Trial3K[2] = new TH1F("hPhot1Trial3K_2","",100,0,0.3);
+    hPhot1Trial3KAvg = new TH1F("hPhot1Trial3KAvg","",100,0,0.3);
+
+    
     // Single-PE fitting related histograms
     hPhot1Q0 = new TH1F("hPhot1Q0","",100,-1000,1000);
     hPhot1T0 = new TH1F("hPhot1T0","",100,-1000,1000);
@@ -252,11 +364,18 @@ namespace trigscint {
     hPhot1Ped = new TH1F("hPhot1Ped","",100,-1000,1000);
     hPhot1Chi = new TH1F("hPhot1Chi","",100,0,2000);
     
+    for(int i=0;i<nChannels;i++){
+      hQAvg15[i] = new TH1F(Form("hQAvg15_%i",i),Form("Average Charge distribution in 1st 15 time samples [Channel %i];Charge [fC];",i),620,-20,600);
+      hQAvg1530[i] = new TH1F(Form("hQAvg1530_%i",i),Form("Average Charge distribution in last 15 time samples [Channel %i];Charge [fC];",i),620,-20,600);
+    }
+    
     // time = new float[maxTS];
-    zeroes = new float[maxTS];
-    for (int ii=0;ii<maxTS;ii++){
-      time[ii] = 25*ii;
-      zeroes[ii] = 0;
+    time[0]=0.;
+    // zeroes = new float[maxTS];
+    zeroes = new float[15];
+    for (int ii=1;ii<15;ii++){
+      time[ii] = time[ii-1]+25.0;
+      zeroes[ii] = 0.0;
     }
     /*
       int yMax = 50;
@@ -292,6 +411,8 @@ namespace trigscint {
     for (int i=0;i<100;i++)
       hPhot1Pulse[i] = new TH1F(Form("hPhot1Pulse_%i",i),"Event with 25< QAvg15 <45;time sample;Q [fC]",15,0,15);    
     
+    for (int i=0;i<100;i++)
+      hQ400Pulses[i] = new TH1F(Form("hQ400Pulses_%i",i),Form("Event %i with 420< QAvg15 <430;time sample;Q [fC]",i),31,0,31);
     int counter=0;
     for(int c1=1;c1<nChannels;c1++){
       for(int c2=0;c2<c1;c2++){
@@ -305,7 +426,9 @@ namespace trigscint {
       }
     }
     evNb = 0;
-	
+
+    for(int i=0;i<nChannels;i++)
+      hAllPulses[i] = new TH2F(Form("hAllPulses_%i",i),Form("Channel %i;Time sample; Charge[fC]",i),30,0,30,100,-20,4000);
     return;
   }
   
@@ -319,12 +442,6 @@ namespace trigscint {
   Double_t SinglePulseShape(Double_t* ts, Double_t* par) {
     auto pls = new Expo(par[2],par[3]);
     pls->AddPulse(par[1],par[0]);
-
-    // /// Debugging
-    // printf("%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\n",ts[0],par[0],par[1],par[2],par[3],par[4]);
-    // printf("Integral: %.2f\n",pls->Integrate(ts[0],ts[0]+25));
-    // ///
-    
     return par[4]+pls->Integrate(ts[0],ts[0]+25);
   }
   
@@ -332,17 +449,31 @@ namespace trigscint {
     Double_t cost{0};
     auto pls = new Expo(par[2],par[3]);
     pls->AddPulse(par[1],par[0]);
+    
 
     for(int i=0;i<15;i++){
       double Qpred = pls->Integrate(time[i],time[i]+25);
-      if(charge[i]<par[4]+3*par[5])
-	cost += pow(Qpred,2);
-      else
-	cost += pow(Qpred-charge[i]+par[4],2);
+      printf("x = %f\tf(x) = %f\tQpred = %f\tQData = %f\n",time[i],pls->EvalSingle(time[i],0),Qpred,charge[i]); // Debugging
+      cost += pow(Qpred-charge[i],2);
       // std::cout<<std::setw(10)<<Qpred<<"\t|";
     }
+    // for(int i=0;i<6;i++)
+    //   printf("%f\t",par[i]);
+    std::cout<<cost<<"\n";
+    
     return cost;
   }
+
+  // /*
+  //  * @brief Fit a single pulse
+  //  * @param Qi An array of 6 floats, with Qi[1] being the charge maximum
+  //  * Qi[i] = charge observed in (i-1) time sample after charge maximum
+  //  * The algorithm will find 5 parameters: Q0,to,k,tmax,pedestal
+  //  */
+  // void QIEAnalyzer::FitSinglePulse(Float_t* Qi) {
+  //   Float_t k = log(Qi[2]/Qi[3])/25.0;
+  //   Float_t A = (Qi[2]-Qi[3])/(exp(-)-exp());
+  // }
 }
 
 DECLARE_ANALYZER_NS(trigscint, QIEAnalyzer)
