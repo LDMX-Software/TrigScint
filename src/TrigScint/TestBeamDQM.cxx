788efa5e12982e5a03c3aa4b0061ede8a8cfbc3e
awhitbeck <whitbeck.andrew@gmail.com>
2022-09-15 22:02:34 -0500

    dhruvanshu edits

--- src/TrigScint/TestBeamDQM.cxx
+++ src/TrigScint/TestBeamDQM.cxx
@@ -6,6 +6,7 @@
 
 #include "TrigScint/TestBeamDQM.h"
 
+
 namespace trigscint {
 
   TestBeamDQM::TestBeamDQM(const std::string& name,
@@ -17,14 +18,12 @@ namespace trigscint {
 
     inputCol_  = parameters.getParameter< std::string >("inputCollection");
     inputPassName_  = parameters.getParameter< std::string >("inputPassName");
-    peds_  = parameters.getParameter< std::vector<double> >("pedestals");
     startSample_  = parameters.getParameter< int >("startSample");
 
     std::cout << " [ TestBeamDQM ] In configure(), got parameters " 
 	      << "\n\t inputCollection = " << inputCol_
 	      << "\n\t inputPassName = " << inputPassName_
 	      << "\n\t startSample = " << startSample_
-	      << "\n\t pedestals[0] = " << peds_[0]
 	      << "\t." << std::endl;
 
     return;
@@ -32,20 +31,24 @@ namespace trigscint {
 
   void TestBeamDQM::analyze(const framework::Event &event) {
 
-    const auto channels{event.getCollection<trigscint::TrigScintQIEDigis >(inputCol_, inputPassName_)};
-
-	int evNb = event.getEventNumber();
-	int nChan = channels.size();
-	
-	for (auto chan : channels) {
-	  int bar = chan.getBarID();
-	  float PE = chan.getPE();
-	  hPEVsDelta[leadBar]->Fill(leadBar-subleadBar, peLead);
-	  hDeltaPEVsDelta[leadBar]->Fill(leadBar-subleadBar, peLead-peSublead);
-	}
-	hPEmaxVsDelta->Fill(leadBar-subleadBar, peLead);
-
-	return;
+    std::cout << "start analyze" << std::endl;
+    int evNb = event.getEventNumber();
+    std::cout << "events: " << evNb << " channels: " << nChannels << std::endl;
+    
+    const auto channels{event.getCollection<trigscint::TrigScintQIEDigis >(inputCol_)};//, inputPassName_)};
+    std::cout << "got collection" << std::endl;
+    int nChannels = channels.size();
+    
+    for (auto chan : channels) {
+      //int bar = chan.getBarID();
+      int bar = chan.getChanID();
+      std::cout << "chan: " << bar << std::endl;
+      std::vector<int> adc = chan.getADC();
+      //int adc = chan.getADC();
+    }
+    //hPEmaxVsDelta->Fill(leadBar-subleadBar, peLead);
+    
+    return;
   }
 
   void TestBeamDQM::onFileOpen() {
@@ -72,26 +75,33 @@ namespace trigscint {
 	float Qmin=-10;
 	int nQbins=(Qmax-Qmin)/4;
 	
-	for (int iB = 0; iB<nChannels; iB++) {
-	  hPE[iB]=new TH1F(Form("hPE_chan%i", iB), Form(";PE, chan%i", iB),nPEbins,0,PEmax);
-	  hPEVsDelta[iB]=new TH2F(Form("hPEVsDelta_chan%i", iB), Form(";#Delta_{barID};PE, chan%i has max PE", iB),nChannels+1,-nChannels/2-0.5,nChannels/2+0.5, nPEbins,0,PEmax);
-	  hDeltaPEVsDelta[iB]=new TH2F(Form("hDeltaPEVsDelta_chan%i", iB), Form(";#Delta_{barID};#Delta_PE, chan%i has max PE", iB),nChannels+1,-nChannels/2-0.5,nChannels/2+0.5, nPEbins,0,PEmax);
+	for (int iB=0; iB<nChannels; iB++) {
+		hADC[iB] = new TH1F(Form("hADC_chan%i",iB), Form("; ADC, chan%i", iB),20,0,80);
 	}
+
+	fillNb=0;
+	evNb=0;
+
+	//for (int iB = 0; iB<nChannels; iB++) {
+	  //hPE[iB]=new TH1F(Form("hPE_chan%i", iB), Form(";PE, chan%i", iB),nPEbins,0,PEmax);
+	  //hPEVsDelta[iB]=new TH2F(Form("hPEVsDelta_chan%i", iB), Form(";#Delta_{barID};PE, chan%i has max PE", iB),nChannels+1,-nChannels/2-0.5,nChannels/2+0.5, nPEbins,0,PEmax);
+	  //hDeltaPEVsDelta[iB]=new TH2F(Form("hDeltaPEVsDelta_chan%i", iB), Form(";#Delta_{barID};#Delta_PE, chan%i has max PE", iB),nChannels+1,-nChannels/2-0.5,nChannels/2+0.5, nPEbins,0,PEmax);
+	//}
 
 	//make event displays for events where there are channels with weird event hit PE counts 
-	for (int iE = 0; iE<nEv; iE++) {
-	  for (int iB = 0; iB<nChannels; iB++) { 
-		hOut[iE][iB] = new TH1F(Form("hCharge_chan%i_nv%i", iB, iE), Form(";time sample; Q, chan %i, ev %i [fC]", iB, iE), nTimeSamp,-0.5,nTimeSamp-0.5);
-	  }
-	}
+	//for (int iE = 0; iE<nEv; iE++) {
+	  //for (int iB = 0; iB<nChannels; iB++) { 
+		//hOut[iE][iB] = new TH1F(Form("hCharge_chan%i_nv%i", iB, iE), Form(";time sample; Q, chan %i, ev %i [fC]", iB, iE), nTimeSamp,-0.5,nTimeSamp-0.5);
+	  //}
+	//}
 	
-	hPEmaxVsDelta=new TH2F("hPEmaxVsDelta",";#Delta_{barID};PE, max hit",nChannels,-nChannels/2,nChannels/2, nPEbins,0,PEmax);
-	hEvDisp = new TH2F(Form("hEvDisp_ev%i", nEv), ";Event number; Bar ID; PE", nEv,0.5,nEv+0.5, nChannels,-0.5,nChannels-0.5);
-	hEvDispPE = new TH2F("hEvDispPEcut", ";Event number; Bar ID; PE", nEv,0.5,nEv+0.5, nChannels,-0.5,nChannels-0.5);
+	//hPEmaxVsDelta=new TH2F("hPEmaxVsDelta",";#Delta_{barID};PE, max hit",nChannels,-nChannels/2,nChannels/2, nPEbins,0,PEmax);
+	//hEvDisp = new TH2F(Form("hEvDisp_ev%i", nEv), ";Event number; Bar ID; PE", nEv,0.5,nEv+0.5, nChannels,-0.5,nChannels-0.5);
+	//hEvDispPE = new TH2F("hEvDispPEcut", ";Event number; Bar ID; PE", nEv,0.5,nEv+0.5, nChannels,-0.5,nChannels-0.5);
   
 
-	fillNb = 0;
-	evNb = 0;
+	//fillNb = 0;
+	//evNb = 0;
 	
     return;
   }
