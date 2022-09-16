46122c8b816d2240bd9675ede632f87795c5be8e
awhitbeck <whitbeck.andrew@gmail.com>
2022-09-15 22:04:26 -0500

    tweak

--- python/DQM.py
+++ python/DQM.py
@@ -1,9 +1,13 @@
 from LDMX.Framework import ldmxcfg
-p = ldmxcfg.Process('DQM')
+p = ldmxcfg.Process('bbb')
 import sys
 from LDMX.TrigScint.trigScint import *
+dqm = TestBeamDQM('aaa')
+dqm.inputCollection='decodedQIEUp'
+#dqm.inputPassName='unpack'
+p.maxEvents = 10
+p.sequence = [dqm]
 
-p.maxEvents = 1000
-p.sequence = [TestBeamProducer()]
 p.inputFile = [sys.argv[1]]
 p.outputFiles = [sys.argv[1].replace(".root","_DQM.root")]
+p.histogramFile = "myHistFile.root"
