1b465f04459438705053148faa4c861aec85bfd2
DhruvanshuSan <82689754+DhruvanshuSan@users.noreply.github.com>
2022-10-14 07:02:46 +0530

    Add files via upload

--- exampleConfigs/testChargeAnalyzer.py
+++ exampleConfigs/testChargeAnalyzer.py
@@ -2,13 +2,25 @@ from LDMX.Framework import ldmxcfg
 p = ldmxcfg.Process('plot')
 import sys
 from LDMX.TrigScint.trigScint import ChargeAnalyzer
+from LDMX.TrigScint.trigScint import TestBeamHitAnalyzer
+from LDMX.TrigScint.trigScint import ADCAnalyzer
 #from ROOT import TTree, TFile
 #p.maxEvents = 1
 #p.maxEvents = 15071
-tbh_analyzer = ChargeAnalyzer('test')
-tbh_analyzer.inputCollection = "QIEsamplesUp"
+#p = ldmxcfg.Process('plot')
+tbh_analyzer = TestBeamHitAnalyzer('Hits')
+tbh_analyzer.inputCollection = "testBeamHitsUp"
 tbh_analyzer.inputPassName = ""
-p.sequence = [tbh_analyzer]
+
+Q_analyzer = ChargeAnalyzer('Charge')
+Q_analyzer.inputCollection = "QIEsamplesUp"
+Q_analyzer.inputPassName = ""
+
+ADC_analyzer = ADCAnalyzer('ADC')
+ADC_analyzer.inputCollection = "decodedQIEUp"
+ADC_analyzer.inputPassName = ""
+
+p.sequence = [ADC_analyzer,Q_analyzer,tbh_analyzer]
 p.inputFiles = [sys.argv[1]]
-#p.inputFiles = ['/home/dhruvanshu/ldmx-sw/unpacked_ldmx_captan_out_13-04-2022_00-00-38__187_reformat_30timeSamplesFrom0_linearize_hits_clusters.root']
-p.histogramFile = sys.argv[1].replace(".root","_DQM_Qanalyzed.root")
+p.histogramFile = sys.argv[1].replace(".root","_multiple_analysed.root")
+print("Analyzed")
